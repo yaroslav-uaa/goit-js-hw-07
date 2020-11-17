183ec12e024aad27023c2ad4985e80deb8c454dc
Yaroslav <ypakhachuk@gmail.com>
2020-11-17 14:56:53 +0200

    fix random rgb color

--- js/task-7_8.js
+++ js/task-7_8.js
@@ -16,8 +16,10 @@ function createBoxes(amount) {
     const newBox = document.createElement("div");
     newBox.style.width = `${30 + i * 10}px`;
     newBox.style.height = `${30 + i * 10}px`;
-    newBox.style.backgroundColor =
-      "#" + (((1 << 24) * Math.random()) | 0).toString(16);
+    // 2-ий варіант
+    let r = () => (Math.random() * 256) >> 0;
+    newBox.style.backgroundColor = `rgb(${r()}, ${r()}, ${r()})`;
+    //   "#" + (((1 << 24) * Math.random()) | 0).toString(16);
     newBoxes.push(newBox);
   }
   boxesRef.append(...newBoxes);

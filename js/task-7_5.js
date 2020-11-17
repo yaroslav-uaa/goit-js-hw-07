b4955e89ae5992c0290bee52aef68a977e19fcb6
Yaroslav <ypakhachuk@gmail.com>
2020-11-17 21:49:09 +0200

    last fix)

--- js/task-7_5.js
+++ js/task-7_5.js
@@ -3,9 +3,9 @@ const spanRef = document.getElementById("name-output");
 console.dir(inputRef);
 console.dir(spanRef);
 inputRef.addEventListener("input", (event) => {
-  inputRef.value = event.target.value.split(" ").join("");
+  inputRef.value = event.target.value;
 
   spanRef.textContent =
-    event.target.value === "" ? "незнайомець" : event.target.value;
+    event.target.value === "" ? "незнайомець" : event.target.value.trim();
 });
 console.log(spanRef);
--- js/task-7_6.js
+++ js/task-7_6.js
@@ -4,16 +4,19 @@ const inputValid = +validInputRef.dataset.length;
 // console.log(inputValid);
 
 validInputRef.addEventListener("input", (event) => {
-  validInputRef.value = event.target.value.split(" ").join("");
+  validInputRef.value = event.target.value.trim();
 });
 validInputRef.addEventListener("blur", (event) => {
   validInputRef.classList.add("invalid");
+  validInputRef.classList.remove("valid");
   switch (event.currentTarget.value.length) {
     case inputValid:
-      validInputRef.classList.replace("invalid", "valid");
+      validInputRef.classList.add("valid");
+      validInputRef.classList.remove("invalid");
       break;
     case 0:
       validInputRef.classList.remove("invalid");
+      validInputRef.classList.remove("valid");
       break;
   }
 });
